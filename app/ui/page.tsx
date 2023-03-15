89de148b0789ad43cc5a554450c4ea82f2e99178
kykim00 <jeongsaeki@likelion.org>
2023-03-15 15:15:30 +0900

    practice modal

--- app/ui/page.tsx
+++ app/ui/page.tsx
@@ -1,13 +1,18 @@
 'use client';
+import Modal from '@/components/common/Modal';
 import Popover from '@/components/common/Popover';
+import { useState } from 'react';
 
 interface PageParams {
   params: {};
 }
 
 const Page = ({ params }: PageParams) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const onToggle = () => setIsOpenModal(prev => !prev);
   return (
-    <div>
+    <div style={{ width: '1000px', margin: 'auto' }}>
       <Popover
         content={
           <p>
@@ -18,6 +23,8 @@ const Page = ({ params }: PageParams) => {
       >
         <button>(?)</button>
       </Popover>
+      <button onClick={onToggle}>모달 오픈</button>
+      <Modal isOpen={isOpenModal} title="모달1" onConfirm={onToggle} onClose={onToggle} />
     </div>
   );
 };
---
+++ components/common/Modal.tsx
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled';
+
+interface ModalProps {
+  isOpen: boolean;
+  title: string;
+  onConfirm?: () => void;
+  onClose?: () => void;
+}
+
+const Modal = ({ isOpen, title, onConfirm, onClose }: ModalProps) => {
+  return (
+    <>
+      {isOpen && (
+        <ModalBackground>
+          <ModalContainer>
+            <Title>{title}</Title>
+            <Button onClick={() => onConfirm?.()}>확인</Button>
+            <Button onClick={() => onClose?.()}>취소</Button>
+          </ModalContainer>
+        </ModalBackground>
+      )}
+    </>
+  );
+};
+
+export default Modal;
+const ModalBackground = styled.div`
+  display: block;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #eee;
+  z-index: 1000;
+`;
+const ModalContainer = styled.div`
+  position: fixed;
+  background-color: white;
+  padding: 20px;
+  border-radius: 4px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  color: black;
+  text-align: center;
+`;
+const Button = styled.button`
+  border: none;
+  border-radius: 4px;
+  background-color: orangered;
+  padding: 20px;
+`;
