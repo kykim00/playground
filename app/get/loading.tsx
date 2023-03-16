b5b61795d254d2ae3880a0ec7e3a1df547cf3bda
kykim00 <jeongsaeki@likelion.org>
2023-03-16 10:17:01 +0900

    practice fetch in server component

---
+++ app/get/loading.tsx
@@ -0,0 +1,8 @@
+const Loading = () => (
+  <>
+    <h1>New Page</h1>
+    <div>Loading...</div>
+  </>
+);
+
+export default Loading;
---
+++ app/get/page.tsx
@@ -0,0 +1,54 @@
+import { Suspense } from 'react';
+
+interface PageParams {
+  params: {};
+}
+
+function delay(ms: number) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(console.log(`${ms}ms delay`));
+    }, ms);
+  });
+}
+async function getData() {
+  const data = await fetch('http://localhost:3001/api/hello');
+  await delay(2000);
+  return data.json();
+}
+const Page = async ({ params }: PageParams) => {
+  const data = await getData();
+
+  return (
+    <>
+      <h1>New Page</h1>
+      {/* Suspense not working */}
+      <Suspense fallback={<div>loading...</div>}>
+        {data.map((person: any) => {
+          const { name, phone, email, list, country, region } = person;
+          return (
+            <ul
+              key={name}
+              style={{
+                backgroundColor: 'pink',
+                padding: '20px',
+                marginRight: '10px',
+                display: 'inline-block',
+                color: 'black',
+              }}
+            >
+              <li>name : {name}</li>
+              <li>phone : {phone}</li>
+              <li>email : {email}</li>
+              <li>list : {list}</li>
+              <li>country : {country}</li>
+              <li>region : {region}</li>
+            </ul>
+          );
+        })}
+      </Suspense>
+    </>
+  );
+};
+
+export default Page;
