985bf29a2473ecabcdf16fdd783e0dc8b6b3f41a
kykim00 <jeongsaeki@likelion.org>
2023-03-21 17:18:25 +0900

    practice table sort, pagination, edit

--- workspaces/12/src/pages/react-table/index.tsx
+++ workspaces/12/src/pages/react-table/index.tsx
@@ -1,18 +1,39 @@
 import axios from 'axios';
 import React, { useEffect, useMemo, useState } from 'react';
-import { useSortBy, useTable } from 'react-table';
+import { usePagination, useSortBy, useTable } from 'react-table';
 import { Products } from '../api/table';
 
 export default function RT() {
   const [data, setData] = useState<Products>();
+  const [cpyData, setCpyData] = React.useState<Products>();
+  const [skipPageReset, setSkipPageReset] = React.useState(false);
+
+  const updateMyData = (rowIndex, columnId, value) => {
+    setSkipPageReset(true);
+    setCpyData(old => {
+      old.map((row, index) => {
+        console.log(row, index, rowIndex, columnId, value);
+
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value,
+          };
+        }
+        return row;
+      });
+    });
+  };
   useEffect(() => {
     getData();
   }, []);
+
   const getData = async () => {
     if (data) return;
     try {
       const res = await axios.get<Products>('https://dummyjson.com/products');
       setData(res.data);
+      setCpyData(res.data.products);
     } catch (e) {
       console.log(e);
     }
@@ -27,39 +48,152 @@ export default function RT() {
   const rows = useMemo(() => data?.products ?? [], [data]);
   if (!data) return;
 
-  return <Table columns={columns} data={rows} />;
+  return <Table columns={columns} data={rows} updateMyData={updateMyData} skipPageReset={skipPageReset} />;
 }
 
-function Table({ columns, data }) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data }, useSortBy);
+const EditableCell = ({
+  value: initialValue,
+  row: { index },
+  column: { id },
+  updateMyData, // This is a custom function that we supplied to our table instance
+}: any) => {
+  // We need to keep and update the state of the cell normally
+  const [value, setValue] = React.useState(initialValue);
+
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
+  // We'll only update the external data when the input is blurred
+  // const onBlur = () => {
+  //   updateMyData(index, id, value);
+  // };
+
+  // If the initialValue is changed external, sync it up with our state
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  return <input value={value} onChange={onChange} />;
+};
+
+const Span = ({ value }) => {
+  return <span>{value}</span>;
+};
+const defaultColumn = {
+  Input: EditableCell,
+  Cell: Span,
+};
+
+function Table({ columns, data, updateMyData, skipPageReset }: any) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+      autoResetPage: !skipPageReset,
+      updateMyData,
+      initialState: {
+        pageIndex: 1,
+        pageSize: 15,
+      },
+    },
+    useSortBy,
+    usePagination,
+  );
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              //@ts-ignore
-              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render('Header')}
-                <span>{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}</span>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => (
-                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                //@ts-ignore
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}</span>
+                </th>
               ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map(row => {
+            prepareRow(row);
+
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  const isEditable = cell.column.Header !== 'ID';
+
+                  return <td {...cell.getCellProps()}>{cell.render(isEditable ? 'Input' : 'Cell')}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   );
 }
 // import React from 'react';
