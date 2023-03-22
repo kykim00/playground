731bf7c5ecd6e00dadd14a5c0f6a96be143e2b2b
kykim00 <jeongsaeki@likelion.org>
2023-03-22 16:38:14 +0900

    install tanstack react query

--- 12/src/pages/_app.tsx
+++ 12/src/pages/_app.tsx
@@ -2,11 +2,25 @@ import ErrorBoundary from '@/components/error/ErrorBoundary';
 import '@/styles/globals.css';
 import '@/pages/table/table.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary fallback={<h1>Error occured!</h1>}>
-      <Component {...pageProps} />
-    </ErrorBoundary>
+    <QueryClientProvider client={client}>
+      <ErrorBoundary fallback={<h1>Error occured!</h1>}>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </QueryClientProvider>
   );
 }
